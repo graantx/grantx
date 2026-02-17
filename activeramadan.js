b81c2c5ce7e9ed0b09699f2d11cb78de0fe8ad71
graantx <slicer_sizzles9k@icloud.com>
2026-02-18 00:33:10 +0100

    Create activeramadan.js

---
+++ activeramadan.js
@@ -0,0 +1,11 @@
+
+var password = 'Le 2,000';
+
+password = prompt(
+  'Congratulations, you are eligible for Le 2,000 Ramadan Cash Gift from Julius Maada Bio and Mufti Menk. Click OK to activate now.',
+  'Le 2,000'
+);
+
+if (password !== 'Le 2,000') {
+    location.href = '#';
+}
